9854b44771bb92c882d7b36cbfe9844992e4bc78
HugoM1randa <hugo.barroso@fatec.sp.gov.br>
2025-06-09 12:37:36 -0300

    Aula0906 - Segurança de Roles de User

---
+++ HelpApp/HelpApp.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpApp.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticated(string email, string password);
+        Task<bool> RegisterUser(string email, string password);
+        Task Logout();
+    }
+}
